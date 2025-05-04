4cd403e6634c3f3be7ede098cde95f203de7ec66
AM3KCodes <161411160+AM3KCodes@users.noreply.github.com>
2025-05-04 00:31:27 -0400

    actually finished

--- pass_gen.py
+++ pass_gen.py
@@ -2,35 +2,85 @@ import random
 import string
 from tkinter import *
 
-# create password based on user input
+# create password based on user input 
 
-def generator(length, isc):
-    if isc==False:
+def generator(length, isc, ist, position, specific_term):
+    
+    # If specific term...
+    if ist and position == "START":
+        length -= len(specific_term)
+        # If special chars...
+        if isc:
+            password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+        # If no special chars...
         password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-        return password
-    if isc==True:
+        return specific_term + password
+    
+    if ist and position == "END":
+        length -= len(specific_term)
+        if isc:
+            password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+        password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+        return password + specific_term
+    
+    # If there is no specific term, check for special chars...
+    if isc:
         password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
-        return password
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return password
+
 
 while True:
+    
+    # CREATING PASSWORD LENGTH
+    
     try:
         length = int(input("Enter the length of your password: "))
         if length <= 0:
             print("Length less than 1, exiting application.")
             break
+        elif length > 30:
+            print("Length cannot be greater than 30. Please enter another value.")
+            continue
     except ValueError: # check for invalid inputs
         print("Please enter a valid number.")
         continue
     
-    option = input("Include special characters? (Y/N): ").upper()
-    if (option=="Y"):
+    
+    # CREATING SPECIFIC TERM
+    
+    incl_specific_term = input("Include specific term? (Y/N): ").strip().upper() # specific term check
+    if (incl_specific_term=="Y"):
+        ist = True
+        specific_term = input("Enter your specific term: ") # enter term
+        if len(specific_term) >= length:
+            print("Specific term's length cannot exceed length of the password. Try again.")
+            continue
+        position = input("Enter the position of the specific term (START/END): ").strip().upper() # position of specific term
+        
+    elif (incl_specific_term=="N"):
+        ist = False
+        specific_term = ""
+        position = "START"
+    else:
+        print("Invalid input. Enter Y or N.")
+        continue
+    
+    
+    # INCLUDING SPECIAL CHARACTERS
+    
+    special_characters = input("Include special characters? (Y/N): ").strip().upper() # special characters boolean check
+    if (special_characters=="Y"):
         isc = True
-    elif (option=="N"):
+    elif (special_characters=="N"):
         isc = False
     else:
         print("Invalid input. Enter Y or N.")
         continue
-        
-    print("Password: ", generator(length, isc))
+    
+    
+    # FINAL PASSWORD
+    
+    print("Password: ", generator(length, isc, ist, position, specific_term))
     break
         
---
+++ path_gen_gui.py
@@ -0,0 +1,105 @@
+from tkinter import *
+import string, random
+
+window = Tk()
+window.title("Random Password Generator")
+window.geometry("800x500")
+window.resizable(False, False)
+window.configure(bg='lightblue')
+
+pass_label = Label(window, text="Your password: ")
+pass_label.pack(side=BOTTOM)
+
+def istEntry():
+    if ist.get() == True:
+        st_text.pack(side=TOP)
+        st_entry.pack(side=TOP)
+        st_text2.pack(side=TOP)
+        st_option.pack(side=TOP)
+    else:
+        st_text.pack_forget()
+        st_entry.pack_forget()
+        st_text2.pack_forget()
+        st_option.pack_forget()
+    
+        
+def passwordGen(length, isc, ist, selected_position, term):
+    if length > 30:
+        pass_label.configure(text="Password length is greater than 30. Please choose another length.")
+        return
+    else:
+        if ist and selected_position == "START":
+            length -= len(term)
+            # If special chars...
+            if isc:
+                password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+            # If no special chars...
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+            
+            final_pass = term + password
+            print(final_pass)
+            pass_label.configure(text=("Your password: " + final_pass))
+            return final_pass
+        
+        if ist and selected_position == "END":
+            length -= len(term)
+            if isc:
+                password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+            password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+            
+            final_pass = password + term
+            print(final_pass)
+            pass_label.configure(text=("Your password: " + final_pass))
+            return final_pass
+    
+    # If there is no specific term, check for special chars...
+    if isc:
+        password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=length))
+    password = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    
+    pass_label.configure(text=("Your password: " + password))
+    print(password)
+    return password
+
+positions = ["START", "END"]
+
+Label(window, text="Welcome to Random Password Generator!", font=("Verdana", 15)).pack(side=TOP)
+# Password length enter...
+Label(window, text="Password Length (up to 30): ", font=("Bahnschrift", 17)).pack(side=TOP)
+# Enter here...
+
+length = IntVar()
+pass_len = Entry(window, width=5, textvariable=length)
+pass_len.pack(side=TOP)
+
+# Include special characters? Checkbox...
+isc = BooleanVar(value=False)
+icl_spec = Checkbutton(window, text="Include special characters?", font=("Bahnschrift", 17), variable=isc)
+icl_spec.pack(side=TOP)
+
+# Include specific term? Checkbox...
+ist = BooleanVar(value=False)
+icl_terms = Checkbutton(window, text="Include specific term?", font=("Bahnschrift", 17), variable=ist, command=istEntry)
+icl_terms.pack(side=TOP)
+
+# Entry window for when box is checked...
+st_text = Label(window, text="Enter term below:", font=("Bahnschrift", 15))
+term = StringVar()
+st_entry = Entry(window, width=15, textvariable=term)
+
+# Positions...
+selected_position = StringVar()
+selected_position.set(positions[0])
+st_text2 = Label(window, text="Position term at start or end of password?", font=("Bahnschrift", 15))
+st_option = OptionMenu(window, selected_position, *positions)
+st_text.pack_forget()
+st_entry.pack_forget()
+st_text2.pack_forget()
+st_option.pack_forget()
+
+
+# Generate button...
+button = Button(window, text="Generate", fg="green", command=lambda: passwordGen(length.get(), isc.get(), ist.get(), selected_position.get(), term.get()))
+button.pack(side=BOTTOM, anchor=CENTER, pady=20)
+
+window.mainloop()
